5cb84b49979a2f5d4e67c49c4d4d669365681edc
Paul Nelson <ultrono@gmail.com>
2024-02-10 10:48:30 +0100

    basic key-based flow implemented

--- game.el
+++ game.el
@@ -26,20 +26,32 @@
 
 (defvar *my-game-board* nil "2D list representing the game board.")
 (defvar *my-game-solution* nil)
-(defvar *my-game-rules* nil "List of transformation rules.")
-(defconst *key-string* "asdfjkl123" "String to index the transformation rules and key bindings.")
+(defvar *my-game-rule-dict* nil "Map from keys to transformation rules.")
 (defvar *my-game-random* nil)
 
-(defun game-action (i)
+(defun symbol-to-char-symbols (sym)
+  "Split a symbol into a list of character symbols."
+  (mapcar #'intern (split-string (symbol-name sym) "" t)))
+
+(defun rule-list-from-dict (dict)
+  "Return a list of rules from DICT."
+  (cl-reduce #'append (mapcar #'cdr dict)))
+
+(defun game-action (a)
   "Apply the I-th rule to the game board."
   (interactive)
   (when (get-text-property (point) 'game)
-    (setq *my-game-board*
-          (apply-rule
-           (nth i *my-game-rules*)
-           (get-text-property (point) 'row)
-           (get-text-property (point) 'col)
-           *my-game-board*))
+    (let ((row (get-text-property (point) 'row))
+          (col (get-text-property (point) 'col)))
+      (if-let ((rule
+                (cl-some
+                 #'(lambda (rule)
+                     (when (check-rule rule row col *my-game-board*)
+                       rule))
+                 (cdr (assoc a *my-game-rule-dict*)))))
+          (setq *my-game-board*
+                (apply-rule rule row col *my-game-board*))
+        (message "No rule found.")))
     (draw-board)
     (if (check-board-cleared)
         (progn
@@ -50,32 +62,35 @@
                 (setq *my-game-random* (1+ *my-game-random*))
                 (let ((height (length *my-game-board*))
                       (width (length (nth 0 *my-game-board*))))
-                  (random-game height width *my-game-rules* *my-game-random*)))
+                  (random-game height width *my-game-rule-dict* *my-game-random*)))
             (init-level))
           (if (< *my-game-level* (length *my-game-levels*))
               (init-level)
             (message "You have completed all levels."))))))
-         
+
+(defconst *game-keys* 'abcdefghijklmnopqrstuvwxyz1234567890)
+
 (define-derived-mode my-game-mode special-mode "My Game"
   "Major mode for playing my game."
   (setq buffer-read-only t)
-  ;; bind numeric keys to apply-rule
-  (dotimes (i (min (length *key-string*) (length *my-game-rules*)))
-    (let ((key (substring *key-string* i (+ i 1))))
-      (define-key
-       my-game-mode-map
-       (kbd key)
-       (lambda ()
-         (interactive)
-         (game-action i))))))
+  ;; bind keys to apply-rule
+  (dolist (a (symbol-to-char-symbols *game-keys*))
+    (define-key my-game-mode-map (kbd (symbol-name a))
+                (lambda ()
+                  (interactive)
+                  (game-action a)))))
 
-(defun init-game (board rules)
+(defun init-game (board rule-dict)
   "Initialize the game with a 2D list representing BOARD and a list of RULES."
   (setq *my-game-board* board)
-  (setq *my-game-rules* rules)
+  (setq *my-game-rule-dict* rule-dict)
   (switch-to-buffer "*My Game*")
   (my-game-mode)
   (draw-board))
+
+(defun init-game-expand (abbr-board abbr-rule-dict)
+  (init-game (expand-board abbr-board)
+             (expand-rule-dict abbr-rule-dict)))
 
 (defun draw-board ()
   "Draw the game board in a new buffer."
@@ -103,11 +118,7 @@
         (when *my-game-random*
           (insert (format "\nDepth: %s\n" *my-game-random*)))
         (insert "\nAvailable transformations:\n")
-        (dotimes (i (length *my-game-rules*))
-          (let ((rule (nth i *my-game-rules*)))
-            (insert (format "%s:\n\t%s -> %s\n"
-                            (substring *key-string* i (+ i 1))
-                            (car rule) (cdr rule)))))
+        (insert-rule-dict *my-game-rule-dict*)
         (goto-char old-point)))))
 
 (defun check-rule (rule row col board)
@@ -166,16 +177,18 @@ Also return the rule applied to get there."
                (move (nth (random (length moves-with-chosen-rule)) moves-with-chosen-rule)))
           (cons (car move) (cons (cdr move) (cdr path))))))))
 
-(defun random-game (height width rules depth)
+(defun random-game (height width rule-dict depth)
   (setq *my-game-random* depth)
   (let* ((board (empty-board height width))
-         (path (random-path depth rules (cons board nil))))
-    (setq *my-game-solution*
-          (mapconcat
-           (lambda (i)
-             (substring *key-string* i (+ i 1)))
-           (cdr path)))
-    (init-game (car path) rules)))
+         (path (random-path depth
+                            (rule-list-from-dict rule-dict)
+                            (cons board nil))))
+    ;; (setq *my-game-solution*
+    ;;       (mapconcat
+    ;;        (lambda (i)
+    ;;          (substring *key-string* i (+ i 1)))
+    ;;        (cdr path)))
+    (init-game (car path) rule-dict)))
 
 (defun empty-board (height width)
   "Return the empty game board of given HEIGHT and WIDTH."
@@ -186,7 +199,6 @@ Also return the rule applied to get there."
           (push '- row))
         (push row result)))
     result))
-
 
 (defun apply-rule (rule row col board)
   "Apply RULE to the game board at row ROW and column COL."
@@ -208,28 +220,6 @@ Also return the rule applied to get there."
       (message "Cannot apply rule here."))
     result))
 
-(defun my-game ()
-  "Start my game."
-  (interactive)
-  (init-game '((A - - -)
-               (- B - -)
-               (- - A -)
-               (- - A -))
-             '((((A) (A)) . ((B) (B)))
-               (((A A)) . ((B B)))
-               (((A -) (A -)) . ((- A) (- A)))
-               (((- A -) (- A -) (- A -)) . ((A - A) (A - A) (A - A)))
-               (((B)) . ((-)))))
-  (draw-board)
-  (message "To move, call 'apply-rule' with the rule ID and coordinate of the cell to change."))
-
-(defun game--run (game)
-  "Start my game."
-  (interactive)
-  (init-game (car game) (cdr game))
-  (draw-board)
-  (message "To move, call 'apply-rule' with the rule ID and coordinate of the cell to change."))
-
 (defun check-board-cleared ()
   "Check if the game board has been cleared."
   (not (cl-some #'(lambda (row)
@@ -238,16 +228,9 @@ Also return the rule applied to get there."
                              row))
                 *my-game-board*)))
 
-(defun game-run (game-id)
-  "Start my game."
-  (interactive "nGame ID: ")
-  (game--run (nth game-id games)))
-
 (defvar *my-game-level* 0)
 
 (defvar *my-game-levels* nil)
-
-
 
 (defun init-level ()
   "Initialize the current level."
@@ -256,129 +239,78 @@ Also return the rule applied to get there."
   (draw-board)
   (message "To move, call 'apply-rule' with the rule ID and coordinate of the cell to change."))
 
-(defun game-levels-run (levels)
-  "Start my game."
-  (setq *my-game-level* 0)
-  (setq *my-game-levels* (copy-tree some-levels))
-  (init-level))
+(defun expand-board (abbr-board)
+  (mapcar #'symbol-to-char-symbols abbr-board))
 
-(defvar my-rules
-  '((((A - A))
-     . ((- - -)))
-    (((- A -))
-     . ((A - A)))
-    (((A)
-      (-))
-     . ((-)
-        (A)))
-    (((-)
-      (A))
-     . ((A)
-        (-)))
-    (((A A))
-     . ((- -)))))
+(defun expand-rule (abbr-rule)
+  (let ((src (car abbr-rule))
+        (dst (cdr abbr-rule)))
+    (cons
+     (mapcar #'symbol-to-char-symbols src)
+     (mapcar #'symbol-to-char-symbols dst))))
 
-(defvar rules-3
-  '((((A -)
-      (A -))
-     . ((A A)
-        (- -)))
-    (((A A)
-      (- -))
-     . ((A -)
-        (A -)))
-    (((A A)
-      (A B))
-     . ((- -)
-        (- A)))
-    (((- A -))
-     . ((A - A)))
-    (((A)
-      (-))
-     . ((-)
-        (A)))
-    (((-)
-      (A))
-     . ((A)
-        (-)))
-    (((A) (B))
-     . ((-) (-)))
-    (((A B))
-     . ((- -)))
-    (((B))
-     . ((-)))))
+(defun expand-rule-dict (abbr-rule-dict)
+  (mapcar (lambda (x)
+            (cons (car x)
+                  (mapcar #'expand-rule (cdr x))))
+          abbr-rule-dict))
 
-(defvar rules-4
-  '((((x a))
-     . ((- A)))
-    (((- A))
-     . ((A - )))
-    (((-) (A))
-     . ((A) (-)))
-    (((a) (b))
-     . ((-) (-)))
-    (((a b))
-     . ((- -)))
-    (((x))
-     . ((-)))))
+(defvar my-board
+  '(---X
+    -A-A
+    --A-))
 
-(defvar games
-  '(
-    (((A - A -)
-      (- A - A)
-      (A - A -)
-      (- A - A))
-     .
-     ((((A - A)) . ((- - -)))
-      (((- A -)) . ((A - A)))
-      (((A) (-)) . ((-) (A)))
-      (((-) (A)) . ((A) (-)))
-      (((A A)) . ((- -)))))
-    (((- B - B)
-      (- - - -)
-      (B A - -)
-      (- - - -))
-     .
-     ((((- A -))
-       . ((A - A)))
-      (((A)
-        (-))
-       . ((-)
-          (A)))
-      (((-)
-        (A))
-       . ((A)
-          (-)))
-      (((A) (B))
-       . ((-) (-)))
-      (((A B))
-       . ((- -)))
-      (((B))
-       . ((-)))))))
+;; take into account horizontal/vertical space
+(defun insert-rule-dict (rule-dict)
+  (dolist (x rule-dict)
+    (insert (format "%s:\n" (car x)))
+    (dolist (rule (cdr x))
+      (insert "==================\n")      
+      (let ((height (length (car rule)))
+            (half-height (/ (length (car rule)) 2)))
+        (dotimes (i height)
+          (insert (format "%s   %s   %s\n" (nth i (car rule))
+                          (if (eq i half-height) "->" "  ")
+                          (nth i (cdr rule)))))
+        ))
+    (insert "==================\n\n")))
 
-(defvar some-levels
-  '(
-    (((A - A -)
-      (- A - A)
-      (A - A -)
-      (- A - A))
-     .
-     ((((A - A)) . ((- - -)))
-      (((- A -)) . ((A - A)))
-      (((A) (-)) . ((-) (A)))
-      (((-) (A)) . ((A) (-)))
-      (((A A)) . ((- -)))))
-    (((A - A A)
-      (A A - A)
-      (A - A -)
-      (- A - A))
-     .
-     ((((A - A)) . ((- - -)))
-      (((- A -)) . ((A - A)))
-      (((A) (-)) . ((-) (A)))
-      (((-) (A)) . ((A) (-)))
-      (((A A)) . ((- -)))))
-    ))
+(defvar my-rule-dict
+  '((w .
+       (((-
+          X)
+         . (X
+            -))
+        ((-
+          A
+          X)
+         . (A
+            X
+            -))))
+    (s .
+       (((X
+          -)
+         . (-
+            X))
+        ((X
+          A
+          -)
+         . (-
+            X
+            A))))
+    (a .
+       (((-X)
+         . (X-))
+        ((-AX)
+         . (AX-))))
+    (d .
+       (((X-)
+         . (-X))
+        ((XA-)
+         . (-XA))))))
+
+
+
 
 (provide 'game)
 ;;; game.el ends here
