f02bc484db0512c2f7adf5fe9bb9b5f27279983d
Paul Nelson <ultrono@gmail.com>
2024-02-10 09:46:25 +0100

    random games, some experiments

--- game.el
+++ game.el
@@ -25,9 +25,37 @@
 ;;; Code:
 
 (defvar *my-game-board* nil "2D list representing the game board.")
+(defvar *my-game-solution* nil)
 (defvar *my-game-rules* nil "List of transformation rules.")
 (defconst *key-string* "asdfjkl123" "String to index the transformation rules and key bindings.")
+(defvar *my-game-random* nil)
 
+(defun game-action (i)
+  "Apply the I-th rule to the game board."
+  (interactive)
+  (when (get-text-property (point) 'game)
+    (setq *my-game-board*
+          (apply-rule
+           (nth i *my-game-rules*)
+           (get-text-property (point) 'row)
+           (get-text-property (point) 'col)
+           *my-game-board*))
+    (draw-board)
+    (if (check-board-cleared)
+        (progn
+          (message "Congratulations! You have cleared the board.")
+          (setq *my-game-level* (1+ *my-game-level*))
+          (if *my-game-random*
+              (progn
+                (setq *my-game-random* (1+ *my-game-random*))
+                (let ((height (length *my-game-board*))
+                      (width (length (nth 0 *my-game-board*))))
+                  (random-game height width *my-game-rules* *my-game-random*)))
+            (init-level))
+          (if (< *my-game-level* (length *my-game-levels*))
+              (init-level)
+            (message "You have completed all levels."))))))
+         
 (define-derived-mode my-game-mode special-mode "My Game"
   "Major mode for playing my game."
   (setq buffer-read-only t)
@@ -39,28 +67,15 @@
        (kbd key)
        (lambda ()
          (interactive)
-         (when (get-text-property (point) 'game)
-           (setq *my-game-board*
-                 (apply-rule
-                  i
-                  (get-text-property (point) 'row)
-                  (get-text-property (point) 'col)
-                  *my-game-board*))
-           (draw-board)
-           (if (check-board-cleared)
-               (progn
-                 (message "Congratulations! You have cleared the board.")
-                 (setq *my-game-level* (1+ *my-game-level*))
-                 (if (< *my-game-level* (length *my-game-levels*))
-                     (init-level)
-                   (message "You have completed all levels."))))))))))
+         (game-action i))))))
 
 (defun init-game (board rules)
   "Initialize the game with a 2D list representing BOARD and a list of RULES."
   (setq *my-game-board* board)
   (setq *my-game-rules* rules)
   (switch-to-buffer "*My Game*")
-  (my-game-mode))
+  (my-game-mode)
+  (draw-board))
 
 (defun draw-board ()
   "Draw the game board in a new buffer."
@@ -69,12 +84,12 @@
       (let ((old-point (point))
             (inhibit-read-only t))
         (erase-buffer)
-        (insert "  ")
-        (dotimes (j (length (nth 0 *my-game-board*)))
-          (insert (format "%s" j)))
-        (insert "\n")
+        ;; (insert "  ")
+        ;; (dotimes (j (length (nth 0 *my-game-board*)))
+        ;;   (insert (format "%s" j)))
+        ;; (insert "\n")
         (dotimes (i (length *my-game-board*))
-          (insert (format "%s " i))
+          ;; (insert (format "%s " i))
           (let ((row (nth i *my-game-board*)))
             (dotimes (j (length row))
               (insert
@@ -85,6 +100,8 @@
                 'col j
                 'game t)))
             (insert "\n")))
+        (when *my-game-random*
+          (insert (format "\nDepth: %s\n" *my-game-random*)))
         (insert "\nAvailable transformations:\n")
         (dotimes (i (length *my-game-rules*))
           (let ((rule (nth i *my-game-rules*)))
@@ -109,7 +126,7 @@
         (dotimes (j pattern-width)
           (push (nth (+ col j)
                      (nth (+ row i)
-                          *my-game-board*))
+                          board))
                 subgrid-row))
         (push (nreverse subgrid-row) board-subgrid)))
     (setq board-subgrid (nreverse board-subgrid))
@@ -121,24 +138,60 @@
         (to-pattern (cdr rule)))
     (cons to-pattern from-pattern)))
 
-(defun previous-boards (rules board)
-  "Return a list of previous positions of the game board."
+(defun previous-moves (rules board)
+  "Return a list of previous positions of the game board.
+Also return the rule applied to get there."
   (let ((board-height (length board))
         (board-width (length (nth 0 board)))
         results)
     (dotimes (i board-height)
       (dotimes (j board-width)
-        (dolist (rule rules)
-          (if (check-rule rule i j board)
-              (push (cons i j) results))))))
-  )
+        (dotimes (k (length rules))
+          (let ((rule (nth k rules)))
+            (let ((reversed-rule (reverse-rule rule)))
+              (if (check-rule reversed-rule i j board)
+                  (push (cons (apply-rule reversed-rule i j board) k) results)))))))
+    results))
+
+(defun random-path (depth rules board-path)
+  "Return a random previous position of the game board."
+  (if (zerop depth)
+      board-path
+    (let* ((path (random-path (1- depth) rules board-path))
+           (moves (previous-moves rules (car path)))
+           (allowed-rules (cl-remove-duplicates (mapcar #'(lambda (move) (cdr move)) moves))))
+      (when (> (length allowed-rules) 0)
+        (let* ((chosen-rule (nth (random (length allowed-rules)) allowed-rules))
+               (moves-with-chosen-rule (cl-remove-if-not #'(lambda (move) (eq (cdr move) chosen-rule)) moves))
+               (move (nth (random (length moves-with-chosen-rule)) moves-with-chosen-rule)))
+          (cons (car move) (cons (cdr move) (cdr path))))))))
+
+(defun random-game (height width rules depth)
+  (setq *my-game-random* depth)
+  (let* ((board (empty-board height width))
+         (path (random-path depth rules (cons board nil))))
+    (setq *my-game-solution*
+          (mapconcat
+           (lambda (i)
+             (substring *key-string* i (+ i 1)))
+           (cdr path)))
+    (init-game (car path) rules)))
+
+(defun empty-board (height width)
+  "Return the empty game board of given HEIGHT and WIDTH."
+  (let (result)
+    (dotimes (i height)
+      (let (row)
+        (dotimes (j width)
+          (push '- row))
+        (push row result)))
+    result))
 
 
-(defun apply-rule (rule-id row col board)
-  "Apply RULE-ID to the game board at row ROW and column COL. RULE-ID is an index in *my-game-rules*. ROW and COL are indices in *my-game-board*."
+(defun apply-rule (rule row col board)
+  "Apply RULE to the game board at row ROW and column COL."
   (interactive "nRule ID: \nnRow: \nnCol: ")
-  (let* ((rule (nth rule-id *my-game-rules*))
-         (to-pattern (cdr rule))
+  (let* ((to-pattern (cdr rule))
          (pattern-height (length to-pattern))
          (pattern-width (length (car to-pattern)))
          (result (copy-tree board)))
@@ -181,7 +234,7 @@
   "Check if the game board has been cleared."
   (not (cl-some #'(lambda (row)
                     (cl-some #'(lambda (cell)
-                                 (eq cell 'A))
+                                 (not (eq cell '-)))
                              row))
                 *my-game-board*)))
 
@@ -209,6 +262,66 @@
   (setq *my-game-levels* (copy-tree some-levels))
   (init-level))
 
+(defvar my-rules
+  '((((A - A))
+     . ((- - -)))
+    (((- A -))
+     . ((A - A)))
+    (((A)
+      (-))
+     . ((-)
+        (A)))
+    (((-)
+      (A))
+     . ((A)
+        (-)))
+    (((A A))
+     . ((- -)))))
+
+(defvar rules-3
+  '((((A -)
+      (A -))
+     . ((A A)
+        (- -)))
+    (((A A)
+      (- -))
+     . ((A -)
+        (A -)))
+    (((A A)
+      (A B))
+     . ((- -)
+        (- A)))
+    (((- A -))
+     . ((A - A)))
+    (((A)
+      (-))
+     . ((-)
+        (A)))
+    (((-)
+      (A))
+     . ((A)
+        (-)))
+    (((A) (B))
+     . ((-) (-)))
+    (((A B))
+     . ((- -)))
+    (((B))
+     . ((-)))))
+
+(defvar rules-4
+  '((((x a))
+     . ((- A)))
+    (((- A))
+     . ((A - )))
+    (((-) (A))
+     . ((A) (-)))
+    (((a) (b))
+     . ((-) (-)))
+    (((a b))
+     . ((- -)))
+    (((x))
+     . ((-)))))
+
 (defvar games
   '(
     (((A - A -)
@@ -221,8 +334,27 @@
       (((A) (-)) . ((-) (A)))
       (((-) (A)) . ((A) (-)))
       (((A A)) . ((- -)))))
-    )
-  )
+    (((- B - B)
+      (- - - -)
+      (B A - -)
+      (- - - -))
+     .
+     ((((- A -))
+       . ((A - A)))
+      (((A)
+        (-))
+       . ((-)
+          (A)))
+      (((-)
+        (A))
+       . ((A)
+          (-)))
+      (((A) (B))
+       . ((-) (-)))
+      (((A B))
+       . ((- -)))
+      (((B))
+       . ((-)))))))
 
 (defvar some-levels
   '(
