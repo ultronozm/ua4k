722e65ce089f61cfe9d1e320a2339b65052d2ce4
Paul Nelson <ultrono@gmail.com>
2024-02-11 19:52:26 +0100

    make-data.py: goal/void parsing

--- make-data.py
+++ make-data.py
@@ -9,10 +9,14 @@ file.close()
 boards = []
 rules = {}
 docs = {}
+goals = []
+voids = []
 
 temp_board = []
 temp_rule_from = []
 temp_rule_to = []
+temp_goal = None
+temp_void = None
 side_effect = None
 current_rule = None
 
@@ -26,6 +30,18 @@ for line in lines:
             temp_rule_from = []
             temp_rule_to = []
             side_effect = None
+        if temp_goal is not None and len(temp_goal) > 0:
+            goals.append(temp_goal)
+            temp_goal = None
+        if temp_void is not None and len(temp_void) > 0:
+            voids.append(temp_void)
+            temp_void = None
+        continue
+    if line.strip().startswith("GOAL"):
+        temp_goal = []
+        continue
+    if line.strip().startswith("VOID"):
+        temp_void = []
         continue
     if line.strip().startswith("BIND"):
         split_line = line.strip().split()
@@ -37,6 +53,13 @@ for line in lines:
         else:
             print("Rule already exists")
         continue
+    # we're reading the line of a pattern
+    if temp_goal is not None:
+        temp_goal.append(line.strip())
+        continue
+    if temp_void is not None:
+        temp_void.append(line.strip())
+        continue
     if current_rule in rules.keys():
         split_line = line.strip().split()
         temp_rule_from.append(split_line[0])
@@ -47,7 +70,6 @@ for line in lines:
         temp_board.append(line.strip())
 
 if len(temp_board) > 0:
-    # Add board to boards
     boards.append(temp_board)
     temp_board = []
 if len(temp_rule_from) > 0:
@@ -55,7 +77,7 @@ if len(temp_rule_from) > 0:
     temp_rule_from = []
     temp_rule_to = []
         
-result = {'boards': boards, 'rules_dict': rules, 'docs': docs}
+result = {'boards': boards, 'rules_dict': rules, 'docs': docs, 'goals': goals, 'voids': voids}
 
 try:
     with open('gamesData.js', 'r') as json_file:
