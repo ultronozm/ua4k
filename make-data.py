7f3b0d99ffb9e06b3406395ec2e54987c3d79a76
Paul Nelson <ultrono@gmail.com>
2024-02-13 15:35:44 +0100

    implement wildcards

--- make-data.py
+++ make-data.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import itertools
 
 filename = sys.argv[1]
 file = open(filename, "r")
@@ -13,12 +14,30 @@ goals = []
 voids = []
 
 temp_board = []
-temp_rule_from = []
-temp_rule_to = []
 temp_goal = None
 temp_void = None
+
+wildcards_stack = []
+temp_rule_from = []
+temp_rule_to = []
 side_effect = None
 current_rule = None
+
+def wildcard_assignments():
+    # For every pair generate all possible combinations of the characters in the second element for each character in the first
+    lst = wildcards_stack
+    combinations = [list(itertools.product(pair[1], repeat=len(pair[0]))) for pair in lst]
+    # Combine these combinations
+    combined = list(itertools.product(*combinations))
+    dicts = []
+    for combination in combined:
+        d = {}
+        for pair, assignment in zip(lst, combination):
+            for key, value in zip(pair[0], assignment):
+                d[key] = value
+        dicts.append(d)
+    return tuple(dicts)
+
 
 for line in lines:
     if line.strip() == "":
@@ -26,7 +45,11 @@ for line in lines:
             boards.append(temp_board)
             temp_board = []
         if len(temp_rule_from) > 0:
-            rules[current_rule].append([temp_rule_from, temp_rule_to, side_effect])
+            for wildcard_assignment in wildcard_assignments():
+                trans_table = str.maketrans(wildcard_assignment)
+                rule_from = [s.translate(trans_table) for s in temp_rule_from]
+                rule_to = [s.translate(trans_table) for s in temp_rule_to]
+                rules[current_rule].append([rule_from, rule_to, side_effect])
             temp_rule_from = []
             temp_rule_to = []
             side_effect = None
@@ -53,6 +76,13 @@ for line in lines:
         else:
             print("Rule already exists")
         continue
+    if line.strip().startswith("FOR"):
+        split_line = line.strip().split()
+        wildcards_stack.append((split_line[1], split_line[2]))
+        continue
+    if line.strip().startswith("END_FOR"):
+        wildcards_stack.pop()
+        continue
     # we're reading the line of a pattern
     if temp_goal is not None:
         temp_goal.append(line.strip())
@@ -63,7 +93,7 @@ for line in lines:
     if current_rule in rules.keys():
         split_line = line.strip().split()
         temp_rule_from.append(split_line[0])
-        temp_rule_to.append(split_line[1])
+        temp_rule_to.append(split_line[1] if len(split_line) > 1 else split_line[0])
         if len(split_line) > 2:
             side_effect = split_line[2]
     else:
