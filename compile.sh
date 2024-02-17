b9d0250248a1e74c0686a5ae22a9b5931cba5379
Paul Nelson <ultrono@gmail.com>
2024-02-17 08:16:03 +0100

    add compile.sh

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if any argument is given
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+
+for file in "$@"; do
+    python3 ./make-data.py $file
+done
